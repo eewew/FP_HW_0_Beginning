ca900711eff409d67fc52c68647b6a47c99ad460
vovan <vovan@DESKTOP-NMJPF1J>
2023-01-13 13:30:57 +0200

    Add project files.

---
+++ FP_HW_01_Вступ.cpp
@@ -0,0 +1,53 @@
+﻿// FP_HW_01_Вступ.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "\tTask 1\n";
+    cout << "\n";
+    cout << "Was I tough to be around?\n";
+    cout << "Well, maybe you're better now\n";
+    cout << "All because I let you down\n";
+    cout << "I know there's so much I didn't say\n";
+    cout << "I don't want to be that way\n";
+    cout << "I guess it's a price you pay\n";
+    cout << "For giving your trust away\n";
+    cout << "I'm giving mine now\n";
+    cout << "\n";
+    cout << "\"Wallows\"\n";
+    cout << "\n"; 
+    cout << "\n";
+    cout << "\tTask 2\n";
+    cout << "\n";
+    cout << "\tEvery\n";
+    cout << "\t\tHunter\n";
+    cout << "\t\t\tWants\n";
+    cout << "\t\t\t\tTo know\n";
+    cout << "\t\t\t\t\tWhere does\n";
+    cout << "\t\t\t\t\t\tPhasant\n";
+    cout << "\t\t\t\t\t\t\tSit\n";
+    cout << "\n";
+    cout << "\n";
+    cout << "\tTask 3\n";
+    cout << "\n";
+    cout << "Selling puppies.\n";
+    cout << "Breed:Rottweiler.\n";
+    cout << "Call:222-22-22\n";
+    cout << "222\t| 222 |\n";
+    cout << "22\t| 22  |\n";
+    cout << "22\t| 22  |\n";
+}
+
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
